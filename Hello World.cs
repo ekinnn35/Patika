d4b0b93276b873ef17db191efaecd165c179c8c4
ekinnn35 <ekinn.9193@gmail.com>
2024-12-16 14:00:58 +0300

    Create Hello World.cs

---
+++ Hello World.cs
@@ -0,0 +1,2 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Ekin OZTURK" + " " + "Patika Bootcamp'inde olma nedenim, yillarca isteyip asla firsat yaratamamis olmam ve Patika Bootcamp'e guveniyor olusum!");
